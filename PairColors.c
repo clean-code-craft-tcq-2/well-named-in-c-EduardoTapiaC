f932c3e12acf724182716ac74a241e87223baf3e
Killerpug <eduardhho@gmail.com>
2022-01-11 14:16:53 -0600

    Added PairColors module
    - splitted test from module and header from code.

---
+++ PairColors.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "PairColors.h"
+
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet", "Invalid"};
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]) - 1;
+
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate", "Invalid"};
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]) - 1;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    //handle invalid data because it may cause unwanted result
+    if(pairNumber < 0 || pairNumber > MAX_COLORPAIRS) {
+        colorPair.majorColor = INVALID;
+        colorPair.minorColor = INVALID;
+        return colorPair;
+    }
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    int pairNumber = colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
+    //handle invalid data because it may cause unwanted result
+    if(pairNumber < 0 || pairNumber > MAX_COLORPAIRS) {
+        return -1;
+    }
+    return pairNumber;
+}
+
+void PrintColorCodeManual() {
+    int pairNumber = 1;
+    for(int MajorColor = 0; MajorColor < numberOfMajorColors; MajorColor++) {
+        for(int MinorColor = 0; MinorColor < numberOfMinorColors; MinorColor++) {
+            printf("pair number %i: %s-%s \n", pairNumber, MajorColorNames[MajorColor], MinorColorNames[MinorColor]);
+            pairNumber++;
+        }
+    }
+}
---
+++ tests.c
@@ -0,0 +1,40 @@
+#ifndef TESTS_C
+#define TESTS_C
+#include <stdio.h>
+#include <assert.h>
+#include "PairColors.h"
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
+
+
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testNumberToPair(-1, INVALID, INVALID);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(INVALID, INVALID, -1);
+    testPairToNumber(VIOLET, SLATE, 25);
+    PrintColorCodeManual();
+    return 0;
+}
+
+#endif
